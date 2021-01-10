e0df7f2736606d468881e397935f1f87dd1385d5
Shashank <s_shashank@outlook.com>
2021-01-10 23:06:24 +0530

    app and custom font setup

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Home from "./screens/Home";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Roboto_400Regular as robotoRegular,
+  Roboto_700Bold as robotoBold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    robotoRegular,
+    robotoBold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <Home />;
+}
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { globalStyles } from "../styles/global";
+
+const Home = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
